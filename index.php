222ff24923fdf319e241aa9f13c6bbd4c8f9ebe3
41324870 <41324870@087857-P33S105.macklabs.br>
2014-09-19 08:44:44 -0300

    criação do arquivo index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            
+        </title>
+    </head>
+    <body>
+        
+    </body>
+</html>
+
+        <?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
