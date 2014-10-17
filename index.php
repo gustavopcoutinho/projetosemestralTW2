5a47067cc897c64192c93800311bd9035f7e7d2a
31442897 <31442897@087868-P33S105.macklabs.br>
2014-10-17 09:05:27 -0300

    index html

--- index.php
+++ index.php
@@ -1,16 +1,78 @@
-<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>OnMack - Início</title>
-    </head>
-    <body>
-        <header>
-            
-        </header>
-        <?php
-
-             ?>
-    </body>
+	<head>
+		<title> OnMack </title>
+		<meta charset="utf-8">
+		<link href="css/homepage.css" type="text/css" rel="stylesheet"/>
+		
+		
+	<body>
+		<header>
+		<nav id="menu1">
+			<ul class="menu">
+				<li><a href="homepage.html" target=""> HOME </a></li>
+				<li><a href="historia.html" target=""> CADASTRO </a></li>
+				<li><a href="carreira.html" target=""> LOGIN </a></li>
+				<li><a href="cartel.html" target=""> SOBRE NÓS </a></li>
+				<li><a href="treinadores.html" target="">  DÚVIDAS FREQUENTES</a></li>
+				
+		</nav>
+		</header>
+		
+	<section id="cor">
+		<img class="foto" src="download.jpg" alt="logo"/>  </br>
+		
+		
+	
+	<section id="esquerda">
+		<h2 class="texto" > CADASTRE-SE! </h2> 
+		<img class="mma" src="logo.jpg" alt="img1"/>
+		<div class="textimg1">Não perca tempo! Entre na nova rede social para os Mackenzistas!</div>	
+		<a class="a1" href="mma.html" target="blank"> Fazer cadastro </a>
+	</section>	
+	
+	<section id="centro">
+		<h2 class="texto">SOBRE NÓS</h2>
+		<img class="chad" src="images.jpg" alt="img2"/>
+		A OnMack é uma rede social para os Mackenzistas interagirem entre si.
+		<a class="a2" href="noticia.html" target="_blank"> Saiba mais! </a>
+	</section>
+	
+	<section id="direita">
+		<h2 class="texto"> LOGIN </h2>
+		<img class="cinta" src="logo2.jpg" alt="img3"/>
+		Já é cadastrado? Não perca tempo e venha interagir conosco!
+		<a class="a3" href="galeria.html" target="blank"> Entrar </a>
+	</section>
+			<div style="clear:both"> </div>
+	
+	<section id="baixo">
+		<h3 class="bigtext"> BEM VINDO! </h3>
+		<p> As redes sociais são muito presentes na vida dos universitarios, e para isso a OnMack surgiu para inovar, sendo a primeira rede social para universitarios, e é claro, só poderia ser do Mackenzie! Venha interagir com outros mackenzistas e tornar essa Universidade cada vez melhor!</p>
+	
+	</section>
+	
+	<section id="baixodir">
+		<h4 class="textoo"> QUEM SOMOS? </h4>
+			Esse site foi criado por um grupo de amigos do Mackenzie, estudantes de Sistemas de Informação e Ciencia da Computação.
+		
+			<div style="clear:both"></div>
+		<h4 class="midias"> FALE CONOSCO! </h4>
+		<a href="fale.html" target=""><img src="img6.png" alt="img"/></a>
+	</section>
+		<div style="clear:both"> </div>
+	
+	<footer>
+		<nav id="menu2">
+			<ul class="menuu">
+		
+				<li><a href="homepage.html" target=""> HOME </a></li>
+				<li><a href="historia.html" target=""> CADASTRO </a></li>
+				<li><a href="carreira.html" target=""> LOGIN </a></li>
+				<li><a href="cartel.html" target=""> SOBRE NÓS </a></li>
+				<li><a href="treinadores.html" target="">  DUVIDAS FREQUENTES</a></li>
+		<h4>Copyright 2014. OnMack. Todos os direitos reservados.</h4> 
+	</footer>
+	</section>
+	
+</body>
 </html>
-
